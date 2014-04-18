d5b36126fe161270a90ee721fcec0b2bf0dc315e
g705346 <laptop@office.com>
2014-04-18 17:06:19 +0800

    test

--- yataixinyu/NTFRemoteCache/src/com/syniverse/xroads/notification/model/remotedata/MCTUserInfo.java
+++ yataixinyu/NTFRemoteCache/src/com/syniverse/xroads/notification/model/remotedata/MCTUserInfo.java
@@ -3,5 +3,6 @@ package com.syniverse.xroads.notification.model.remotedata;
 import java.io.Serializable;
 
 public class MCTUserInfo implements Serializable{
+	private static final long serialVersionUID = 1L;
   
 }
