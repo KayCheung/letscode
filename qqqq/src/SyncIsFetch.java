3284341ad27784e5051c218000f3fd8486ac6402
Administrator <Administrator@WIN-39C8RGH1KDO>
2014-04-18 23:46:53 +0800

    a

--- qqqq/src/SyncIsFetch.java
+++ qqqq/src/SyncIsFetch.java
@@ -2,5 +2,5 @@
 public class SyncIsFetch {
 // qqqqq
 // ubuntu modify later, ubuntu does not know win7 has push a new modification
-// Win7 modify after ubuntu's modification
+// Win7 modify after ubuntu's modificationlll
 }
