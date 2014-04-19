875236bb85e35e404163961d3a2133cc6a53700d
marvin263 <marvin263@163.com>
2014-04-19 16:35:55 +0800

    a

--- qqqq/src/SyncIsFetch.java
+++ qqqq/src/SyncIsFetch.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
-//devbranch modifies first
->>>>>>> devbranch
+
 public class SyncIsFetch {
 // ubuntu modify later, ubuntu does not kndddow win7 has push a new modification
 // Win7 modify after ubuntu's modification
