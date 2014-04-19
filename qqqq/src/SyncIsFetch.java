8175badf66379eaa3abbcc0db216772940c2efa6
marvin263 <marvin263@163.com>
2014-04-19 11:15:36 +0800

    aaaaaa

--- qqqq/src/SyncIsFetch.java
+++ qqqq/src/SyncIsFetch.java
@@ -1,4 +1,4 @@
-//marvinNewBranch
+////marvinNewBranch
 public class SyncIsFetch {
 // ubuntu modify later, ubuntu does not kndddow win7 has push a new modification
 // Win7 modify after ubuntu's modification
