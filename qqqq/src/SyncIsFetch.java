bf8983119d2957b1783cfd9a7027a34cbdfa90af
marvin263 <marvin263@163.com>
2014-04-19 13:14:26 +0800

    dddd

--- qqqq/src/SyncIsFetch.java
+++ qqqq/src/SyncIsFetch.java
@@ -1,4 +1,4 @@
-// on master, modify, NOT add to index, NOT commit, append on devbranch
+// Edit on devbranch 
 public class SyncIsFetch {
 // ubuntu modify later, ubuntu does not kndddow win7 has push a new modification
 // Win7 modify after ubuntu's modification
