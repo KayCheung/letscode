01f0c7c03f07e33d14669c4cd57e3039deb2906c
marvin263 <marvin263@163.com>
2014-04-19 12:47:08 +0800

    on master we've made changes, but we commit it in devbranch

--- qqqq/src/SyncIsFetch.java
+++ qqqq/src/SyncIsFetch.java
@@ -1,4 +1,4 @@
-// Edit on master, then add to index. But, NOT commit
+// On master, do modification, add to index. But, not commit
 public class SyncIsFetch {
 // ubuntu modify later, ubuntu does not kndddow win7 has push a new modification
 // Win7 modify after ubuntu's modification
