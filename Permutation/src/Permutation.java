9cee31a49d1a579e1549675af4842fbcc327d0f7
g705346 <g705346@AP6H1T9W1.ad.syniverse.com>
2014-08-01 17:56:34 +0800

    k

--- Permutation/src/Permutation.java
+++ Permutation/src/Permutation.java
@@ -10,22 +10,36 @@ public class Permutation<E> {
 	public void ffdfd(LinkedList<E> remainingElement,
 			ArrayList<LinkedList<E>> listExistingAlignment) {
 		E header = cutHeader(remainingElement);
-		
+		if (header == null) {
+			return;
+		}
+
+		if (listExistingAlignment.size() == 0) {
+			LinkedList<E> oneAlignment = new LinkedList<E>();
+			oneAlignment.add(header);
+			listExistingAlignment.add(oneAlignment);
+			return;
+		}
+
 		int existedAlignmentCount = listExistingAlignment.size();
 		for (int i = 0; i < existedAlignmentCount; i++) {
 			LinkedList<E> oneAlignment = listExistingAlignment.get(i);
 			int currentAligmentSize = oneAlignment.size();
-			
+
 			int spawnAlignmentCount = currentAligmentSize + 1;
-			
-			LinkedList<E>[] dd= new LinkedList<E>[]; 
-			
-			
+
+			LinkedList<E>[] dd = new LinkedList<E>[];
+
 		}
 		for (LinkedList<E> oneAlignment : listExistingAlignment) {
 			int size = oneAlignment.size();
 
 		}
+	}
+
+	private void spawnList(E header, LinkedList<E> orgnAlignment,
+			ArrayList<LinkedList<E>> newExistingAlignment) {
+		
 	}
 
 	private E cutHeader(LinkedList<E> beChanged) {
