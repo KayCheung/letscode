8ec878b87207558114874c5dfbba3610ad326a35
g705346 <g705346@AP6H1T9W1.ad.syniverse.com>
2014-08-04 11:13:45 +0800

    ref

--- Permutation/src/Permutation.java
+++ Permutation/src/Permutation.java
@@ -3,8 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Permutation {
-
-	public static void afdf(byte[] allElement) {
+	public static void generatePermutation(byte[] allElement) {
 		int length = allElement.length;
 
 		List<byte[]> currentContainer = new ArrayList<byte[]>();
@@ -32,6 +31,55 @@ public class Permutation {
 	}
 
 	public static void main(String[] args) {
-		afdf(new byte[] { 1, 2, 3,4,6,7,8,9 });
+		generatePermutation(new byte[] { 1, 2, 3, 4 });
+	}
+
+	/** static helper class */
+	static class AlignmentInfo {
+		private byte[] orgnArray;
+		private int usedCount;
+
+		private AlignmentInfo(byte[] orgnArray, int usedCount) {
+			this.orgnArray = orgnArray;
+			this.usedCount = usedCount;
+		}
+
+		private List<byte[]> spawnAlignment(byte newElement) {
+			System.out.println("newElement=" + newElement + ", orgnArray="
+					+ Arrays.toString(orgnArray));
+			int usedCount = this.usedCount;
+			int orgnLength = orgnArray.length;
+
+			List<byte[]> listContainer = new ArrayList<byte[]>(usedCount + 1);
+			// put newElement at index i
+			for (int i = 0; i < usedCount; i++) {
+				AlignmentInfo aInfo = new AlignmentInfo(new byte[orgnLength], 0);
+				listContainer.add(aInfo.orgnArray);
+
+				// elements before i
+				System.arraycopy(orgnArray, 0, aInfo.orgnArray, 0, i);
+				// elements at i
+				aInfo.orgnArray[i] = newElement;
+				// elements after i
+				System.arraycopy(orgnArray, i, aInfo.orgnArray, i + 1,
+						usedCount - i);
+				aInfo.usedCount++;
+			}
+
+			listContainer.add(orgnArray);
+			orgnArray[usedCount] = newElement;
+			usedCount++;
+
+			for (byte[] aArray : listContainer) {
+				System.out.println(Arrays.toString(aArray));
+			}
+			return listContainer;
+		}
+
+		public static void main(String[] args) {
+			byte[] aaa = new byte[] { 1, 2, 3, 0, 0 };
+			AlignmentInfo aInfo = new AlignmentInfo(aaa, 3);
+			aInfo.spawnAlignment((byte) 4);
+		}
 	}
 }
