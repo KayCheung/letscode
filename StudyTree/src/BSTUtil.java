cca00e2656ea1df7c545ab266d5b5e56bd18723c
marvin263 <marvin263@163.com>
2014-08-20 22:25:23 +0800

    1. AVL的 leftBalance还没搞明白 2. Delete BSTNode 不能删除重复的，还不能删除根。这几个bug要看看哦

--- StudyTree/src/BSTUtil.java
+++ StudyTree/src/BSTUtil.java
@@ -36,6 +36,95 @@ public class BSTUtil {
 	 * @return
 	 */
 	public static Node deleteFromBST(Node root, Node nodeToBeDeleted) {
-		return null;
+		Node parent = findParent(root, nodeToBeDeleted);
+		// nodeToBeDeleted is root
+		if (parent == null) {
+			return null;
+		}
+		// nodeToBeDeleted is a leaf
+		if (nodeToBeDeleted.L == null && nodeToBeDeleted.R == null) {
+			if (parent.L == nodeToBeDeleted) {
+				parent.L = null;
+			} else {
+				parent.R = null;
+			}
+			return root;
+		}
+		// nodeToBeDeleted.L is null (R is NOT null)
+		if (nodeToBeDeleted.L == null) {
+			if (parent.L == nodeToBeDeleted) {
+				parent.L = nodeToBeDeleted.R;
+			} else {
+				parent.R = nodeToBeDeleted.R;
+			}
+			return root;
+		}
+		// nodeToBeDeleted.R is null (L is NOT null)
+		if (nodeToBeDeleted.R == null) {
+			if (parent.L == nodeToBeDeleted) {
+				parent.L = nodeToBeDeleted.L;
+			} else {
+				parent.R = nodeToBeDeleted.L;
+			}
+			return root;
+		}
+		// neither nodeToBeDeleted.L nor nodeToBeDeleted.R is null
+		Node[] arraySuccessor = findSuccessor(nodeToBeDeleted);
+
+		Node successorParent = arraySuccessor[0];
+		Node successor = arraySuccessor[1];
+
+		if (successorParent == nodeToBeDeleted) {
+			if (parent.R == nodeToBeDeleted) {
+				parent.R = successor;
+				successor.L = nodeToBeDeleted.L;
+			} else {
+				parent.L = successor;
+				successor.L = nodeToBeDeleted.L;
+			}
+		} else {
+			if (parent.R == nodeToBeDeleted) {
+				parent.R = successor;
+				successor.L = nodeToBeDeleted.L;
+				successorParent.L = successor.R;
+			} else {
+				parent.L = successor;
+				successor.L = nodeToBeDeleted.L;
+				successorParent.L = successor.R;
+			}
+		}
+
+		return root;
+	}
+
+	public static Node findParent(Node root, Node nodeToBeDeleted) {
+		Node parent = null;
+		Node tmp = root;
+		while (tmp != nodeToBeDeleted) {
+			parent = tmp;
+			if (tmp.data < nodeToBeDeleted.data) {
+				tmp = tmp.R;
+			} else {
+				tmp = tmp.L;
+			}
+		}
+		return parent;
+	}
+
+	public static Node[] findSuccessor(Node nodeToBeDeleted) {
+		// No right child at all
+		if (nodeToBeDeleted.R == null) {
+			return null;
+		}
+		Node successorParent = null;
+		Node successor = nodeToBeDeleted;
+		Node tmp = nodeToBeDeleted.R;
+
+		while (tmp != null) {
+			successorParent = successor;
+			successor = tmp;
+			tmp = tmp.L;
+		}
+		return new Node[] { successorParent, successor };
 	}
 }
