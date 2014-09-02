f9eabad130a3123402b64ae58144698b11431645
marvin263 <marvin263@163.com>
2014-09-02 23:26:04 +0800

    1. 就是锻炼思维 2. 反正也不为快慢 3. 看明白算法，自己去实现。看不懂的就看算法书

--- StudyTree/src/com/basicsort/HeapSort.java
+++ StudyTree/src/com/basicsort/HeapSort.java
@@ -10,6 +10,22 @@ public class HeapSort extends AbstractBasicSort {
 		return array;
 	}
 
+	public void ascending_By_BigRoot(int start, int end, int[] array) {
+
+	}
+
+	private void buildBigRootHeap(int start, int end, int[] array) {
+
+	}
+
+	public void descending_By_SmallRoot(int start, int end, int[] array) {
+
+	}
+
+	private void buildSmallRootHeap(int start, int end, int[] array) {
+
+	}
+
 	public static void main(String[] args) {
 		runTest(new HeapSort());
 	}
