f1453cf288d4a856d2f14ae94eabaf51a6776684
marvin263 <marvin263@163.com>
2014-09-06 19:43:27 +0800

    真是前人智慧的结晶，这玩意最初的那些人究竟是怎么想出来的。NB

--- StudyTree/src/com/basicsort/QuickSort.java
+++ StudyTree/src/com/basicsort/QuickSort.java
@@ -1,10 +1,41 @@
 package com.basicsort;
 
+import com.util.CommUtil;
+
 public class QuickSort extends AbstractBasicSort {
 
 	@Override
 	public int[] doSort(int start, int end, int[] array) {
-		return null;
+		quickSort(start, end, array);
+		return array;
+	}
+
+	public void quickSort(int start, int end, int[] array) {
+		if (start >= end) {
+			return;
+		}
+		int pivot = firstElement_Should_Fall_Into(start, end, array);
+		quickSort(start, pivot - 1, array);
+		quickSort(pivot + 1, end, array);
+	}
+
+	private int firstElement_Should_Fall_Into(int low, int high, int[] array) {
+		int firstValue = array[low];
+		while (low < high) {
+			while (low < high && array[high] >= firstValue) {
+				high--;
+			}
+			array[low] = array[high];
+
+			while (low < high && array[low] <= firstValue) {
+				low++;
+			}
+			array[high] = array[low];
+		}
+		array[low] = firstValue;
+		System.out.println("low=" + low + ", high=" + high
+				+ ". First element falls into: " + low);
+		return low;
 	}
 
 	public static void main(String[] args) {
