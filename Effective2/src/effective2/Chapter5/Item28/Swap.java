5308fbf162f470bfe297c794f6ecac97224c7521
g705346 <g705346@AP6H1T9W1.ad.syniverse.com>
2014-10-29 13:54:16 +0800

    Generic method captures wildcard

--- Effective2/src/effective2/Chapter5/Item28/Swap.java
+++ Effective2/src/effective2/Chapter5/Item28/Swap.java
@@ -1,22 +1,25 @@
 package effective2.Chapter5.Item28;
+
 // Private helper method for wildcard capture - Pages 139-140
 
 import java.util.*;
 
 public class Swap {
-    public static void swap(List<?> list, int i, int j) {
-        swapHelper(list, i, j);
-    }
+	public static void swap(List<?> list, int i, int j) {
+		// Marvin: List<?>，除了null，啥都add不进去。怎么解决？
+		// Generic method 来 capture wildcard（好吧，我也不知道怎么算 捕获wildcard）
+		swapHelper(list, i, j);
+	}
 
-    // Private helper method for wildcard capture
-    private static <E> void swapHelper(List<E> list, int i, int j) {
-        list.set(i, list.set(j, list.get(i)));
-    }
+	// Private helper method for wildcard capture
+	private static <E> void swapHelper(List<E> list, int i, int j) {
+		list.set(i, list.set(j, list.get(i)));
+	}
 
-    public static void main(String[] args) {
-        // Swap the first and last argument and print the resulting list
-        List<String> argList = Arrays.asList(args);
-        swap(argList, 0, argList.size() - 1);
-        System.out.println(argList);
-    }
+	public static void main(String[] args) {
+		// Swap the first and last argument and print the resulting list
+		List<String> argList = Arrays.asList(args);
+		swap(argList, 0, argList.size() - 1);
+		System.out.println(argList);
+	}
 }
