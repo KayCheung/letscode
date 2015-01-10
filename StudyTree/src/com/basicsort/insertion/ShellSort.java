08162521853ce0091361f07dd7ed862b3c6ee0d5
marvin <marvin@marvin-965P-S3>
2015-01-10 22:12:35 +0800

    /**  * <pre>  * 稳定性：稳定  *  * 空间复杂度，辅助空间：O(1)  *  * 时间复杂度  *          最坏：O(n^2)  *  *          最好：O(n)  *  *          平均：O(n^2)  * </pre>  *  * @author marvin  *  */

--- StudyTree/src/com/basicsort/insertion/ShellSort.java
+++ StudyTree/src/com/basicsort/insertion/ShellSort.java
@@ -2,6 +2,23 @@ package com.basicsort.insertion;
 
 import com.basicsort.AbstractBasicSort;
 
+/**
+ * <pre>
+ * 稳定性：稳定
+ * 
+ * 空间复杂度，辅助空间：O(1)
+ * 
+ * 时间复杂度
+ *          最坏：O(n^2)
+ * 
+ *          最好：O(n)
+ * 
+ *          平均：O(n^2)
+ * </pre>
+ * 
+ * @author marvin
+ * 
+ */
 public class ShellSort extends AbstractBasicSort {
 
 	@Override
--- StudyTree/src/com/basicsort/selection/SimpleSelectionSort.java
+++ StudyTree/src/com/basicsort/selection/SimpleSelectionSort.java
@@ -3,6 +3,23 @@ package com.basicsort.selection;
 import com.basicsort.AbstractBasicSort;
 import com.util.CommUtil;
 
+/**
+ * <pre>
+ * 稳定性：稳定
+ * 
+ * 空间复杂度，辅助空间：O(1)
+ * 
+ * 时间复杂度
+ *          最坏：O(n^2)
+ * 
+ *          最好：O(n)
+ * 
+ *          平均：O(n^2)
+ * </pre>
+ * 
+ * @author marvin
+ * 
+ */
 public class SimpleSelectionSort extends AbstractBasicSort {
 
 	@Override
