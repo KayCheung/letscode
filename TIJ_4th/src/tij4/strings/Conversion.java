e677853c91e600f7e05d1e2ace03c2baf5192ebe
marvin <marvin@marvin-965P-S3>
2015-01-14 23:04:11 +0800

    w

--- TIJ_4th/src/tij4/strings/Conversion.java
+++ TIJ_4th/src/tij4/strings/Conversion.java
@@ -1,110 +1,84 @@
 package tij4.strings;
+
 //: strings/Conversion.java
-import java.math.*;
-import java.util.*;
+import java.math.BigInteger;
+import java.util.Formatter;
 
 public class Conversion {
-  public static void main(String[] args) {
-    Formatter f = new Formatter(System.out);
+	public static void main(String[] args) {
+		Formatter f = new Formatter(System.out);
+		/** 1. 输出 char **/
+		char u = 'a';
+		System.out.println("u = 'a'");
+		f.format("s: %s\n", u);// 字符串
+		// f.format("d: %d\n", u); // 简直不能更好了。char 不能作为int输出
+		f.format("c: %c\n", u);// unicode输出
+		f.format("b: %b\n", u);// boolean输出
+		// f.format("f: %f\n", u);// char 不能 作为浮点数输出的
+		// f.format("e: %e\n", u);
+		// f.format("x: %x\n", u);
+		Object obj = new Object();
+		System.out.println(Integer.toHexString(obj.hashCode()));
+		f.format("h: %h\n", obj);
 
-    char u = 'a';
-    System.out.println("u = 'a'");
-    f.format("s: %s\n", u);
-    // f.format("d: %d\n", u);
-    f.format("c: %c\n", u);
-    f.format("b: %b\n", u);
-    // f.format("f: %f\n", u);
-    // f.format("e: %e\n", u);
-    // f.format("x: %x\n", u);
-    f.format("h: %h\n", u);
+		/** 2. 输出 int **/
+		int v = 121;
+		System.out.println("v = 121");
+		f.format("d: %d\n", v);
+		f.format("c: %c\n", v);// int可以输出 unicode 啊
+		f.format("b: %b\n", v);
+		f.format("s: %s\n", v);
+		// f.format("f: %f\n", v);// int 不能 作为浮点数输出的
+		// f.format("e: %e\n", v);
+		f.format("x: %x\n", v);
+		f.format("h: %h\n", v);// int竟然也能按着 hashcode 来输出
 
-    int v = 121;
-    System.out.println("v = 121");
-    f.format("d: %d\n", v);
-    f.format("c: %c\n", v);
-    f.format("b: %b\n", v);
-    f.format("s: %s\n", v);
-    // f.format("f: %f\n", v);
-    // f.format("e: %e\n", v);
-    f.format("x: %x\n", v);
-    f.format("h: %h\n", v);
+		/** 3. 输出 BigInteger **/
+		BigInteger w = new BigInteger("50000000000000");
+		System.out.println("w = new BigInteger(\"50000000000000\")");
+		f.format("d: %d\n", w);
+		// f.format("c: %c\n", w);// BigInteger不能 作为 unicode 输出
+		f.format("b: %b\n", w); // boolean/Boolean外的所有的类型；非null，则总是true
+		f.format("s: %s\n", w);
+		// f.format("f: %f\n", w); // BigInteger也不能作为 浮点数 输出的
+		// f.format("e: %e\n", w);
+		f.format("x: %x\n", w);
+		f.format("h: %h\n", w);
 
-    BigInteger w = new BigInteger("50000000000000");
-    System.out.println(
-      "w = new BigInteger(\"50000000000000\")");
-    f.format("d: %d\n", w);
-    // f.format("c: %c\n", w);
-    f.format("b: %b\n", w);
-    f.format("s: %s\n", w);
-    // f.format("f: %f\n", w);
-    // f.format("e: %e\n", w);
-    f.format("x: %x\n", w);
-    f.format("h: %h\n", w);
+		/** 4. 输出 double **/
+		double x = 179.543;
+		System.out.println("x = 179.543");
+		// f.format("d: %d\n", x); //很好，非常好。double 不能作为int输出（int也不能作为double输出）
+		// f.format("c: %c\n", x);// double也不能 作为 unicode 输出
+		f.format("b: %b\n", x);
+		f.format("s: %s\n", x);
+		f.format("f: %f\n", x);
+		f.format("e: %e\n", x);
+		// f.format("x: %x\n", x);//很好，非常好。double不能作为 16进制 int输出
+		f.format("h: %h\n", x);
 
-    double x = 179.543;
-    System.out.println("x = 179.543");
-    // f.format("d: %d\n", x);
-    // f.format("c: %c\n", x);
-    f.format("b: %b\n", x);
-    f.format("s: %s\n", x);
-    f.format("f: %f\n", x);
-    f.format("e: %e\n", x);
-    // f.format("x: %x\n", x);
-    f.format("h: %h\n", x);
+		/** 5. 输出 任意对象 **/
+		Conversion y = new Conversion();
+		System.out.println("y = new Conversion()");
+		// f.format("d: %d\n", y);
+		// f.format("c: %c\n", y);
+		f.format("b: %b\n", y);
+		f.format("s: %s\n", y);// 当然，任何对象，都可以输出为 字符串
+		// f.format("f: %f\n", y);
+		// f.format("e: %e\n", y);
+		// f.format("x: %x\n", y);
+		f.format("h: %h\n", y);
 
-    Conversion y = new Conversion();
-    System.out.println("y = new Conversion()");
-    // f.format("d: %d\n", y);
-    // f.format("c: %c\n", y);
-    f.format("b: %b\n", y);
-    f.format("s: %s\n", y);
-    // f.format("f: %f\n", y);
-    // f.format("e: %e\n", y);
-    // f.format("x: %x\n", y);
-    f.format("h: %h\n", y);
-
-    boolean z = false;
-    System.out.println("z = false");
-    // f.format("d: %d\n", z);
-    // f.format("c: %c\n", z);
-    f.format("b: %b\n", z);
-    f.format("s: %s\n", z);
-    // f.format("f: %f\n", z);
-    // f.format("e: %e\n", z);
-    // f.format("x: %x\n", z);
-    f.format("h: %h\n", z);
-  }
-} /* Output: (Sample)
-u = 'a'
-s: a
-c: a
-b: true
-h: 61
-v = 121
-d: 121
-c: y
-b: true
-s: 121
-x: 79
-h: 79
-w = new BigInteger("50000000000000")
-d: 50000000000000
-b: true
-s: 50000000000000
-x: 2d79883d2000
-h: 8842a1a7
-x = 179.543
-b: true
-s: 179.543
-f: 179.543000
-e: 1.795430e+02
-h: 1ef462c
-y = new Conversion()
-b: true
-s: Conversion@9cab16
-h: 9cab16
-z = false
-b: false
-s: false
-h: 4d5
-*///:~
+		/** 6. 输出 boolean **/
+		boolean z = false;
+		System.out.println("z = false");
+		// f.format("d: %d\n", z);//很好，非常好。boolean不能作为 int
+		// f.format("c: %c\n", z);
+		f.format("b: %b\n", z);
+		f.format("s: %s\n", z);
+		// f.format("f: %f\n", z);
+		// f.format("e: %e\n", z);
+		// f.format("x: %x\n", z);
+		f.format("h: %h\n", z);
+	}
+}
--- TIJ_4th/src/tij4/strings/TestFormatter.java
+++ TIJ_4th/src/tij4/strings/TestFormatter.java
@@ -15,7 +15,7 @@ public class TestFormatter {
 		// %[argument_index$][flags][width][.precision]conversion
 		Formatter f1 = new Formatter(System.out);
 		// 格式化输出字符串和数字
-		f1.format("1. 格式化输出：%s %d\n", "a", 1235);
+		f1.format("1. 格式化输出：%s %d\n", "marvin string", 1235);
 		// 日期的格式化
 		Calendar c = new GregorianCalendar();
 		f1.format("2. 当前日期:%1$tY-%1$tm-%1$te\n", c);
@@ -24,7 +24,8 @@ public class TestFormatter {
 		// 5：最大输出宽度为20,不够会补空格，实际若超过则全部输出
 		// .2：在此表示输出参数2的最大字符数量，如果是浮点数字，则表示小数部分显示的位数
 		// s ：表示输入参数是字符串
-		f1.format("3. %2$-5.2s %1$2s \n", "123", "456");
+		// 注意，输出是：3. AB    abcdefg
+		f1.format("3. %2$-5.2s %1$2s\n", "abcdefg", "ABCEDEFG");
 
 		// 将格式化的结果存储到字符串
 		String fs = String.format("4. 身高体重(%.2f , %d)\n", 173.2, 65);
