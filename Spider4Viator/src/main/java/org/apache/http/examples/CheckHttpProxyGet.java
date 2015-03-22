fdb22bf27f7b987f83b792cd9811aa420596f4cb
marvin <marvin@marvin-965P-S3>
2015-03-22 09:41:59 +0800

    proxy

---
+++ Spider4Viator/src/main/java/org/apache/http/examples/CheckHttpProxyGet.java
@@ -0,0 +1,128 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.http.examples;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import org.apache.http.ConnectionReuseStrategy;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.DefaultBHttpClientConnection;
+import org.apache.http.impl.DefaultConnectionReuseStrategy;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.protocol.HttpCoreContext;
+import org.apache.http.protocol.HttpProcessor;
+import org.apache.http.protocol.HttpProcessorBuilder;
+import org.apache.http.protocol.HttpRequestExecutor;
+import org.apache.http.protocol.RequestConnControl;
+import org.apache.http.protocol.RequestContent;
+import org.apache.http.protocol.RequestExpectContinue;
+import org.apache.http.protocol.RequestTargetHost;
+import org.apache.http.protocol.RequestUserAgent;
+
+import com.studytrails.json.jackson.FetchProductCode;
+
+/**
+ * Elemental example for executing multiple GET requests sequentially.
+ */
+public class CheckHttpProxyGet {
+
+	public static void main(String[] args) throws Exception {
+		HttpProcessor httpproc = HttpProcessorBuilder.create()
+				.add(new RequestContent()).add(new RequestTargetHost())
+				.add(new RequestConnControl())
+				.add(new RequestUserAgent(FetchProductCode.FIRE_USER_AGENT))
+				.add(new RequestExpectContinue(true)).build();
+
+		HttpRequestExecutor httpexecutor = new HttpRequestExecutor();
+
+		HttpCoreContext coreContext = HttpCoreContext.create();
+		HttpHost host = new HttpHost(FetchProductCode.VIATOR_HOST,
+				FetchProductCode.VIATOR_PORT);
+		coreContext.setTargetHost(host);
+
+		DefaultBHttpClientConnection conn = new DefaultBHttpClientConnection(
+				8 * 1024* 1024);
+		ConnectionReuseStrategy connStrategy = DefaultConnectionReuseStrategy.INSTANCE;
+
+		try {
+
+			String[] targets = { "/service/taxonomy/locations?apiKey=7996631481948906" };
+
+			for (int i = 0; i < targets.length; i++) {
+				if (!conn.isOpen()) {
+					Socket socket = new Socket(host.getHostName(),
+							host.getPort());
+					conn.bind(socket);
+					
+				}
+				BasicHttpRequest request = new BasicHttpRequest("GET",
+						targets[i]);
+				System.out.println(">> Request URI: "
+						+ request.getRequestLine().getUri());
+
+				httpexecutor.preProcess(request, httpproc, coreContext);
+				HttpResponse response = httpexecutor.execute(request, conn,
+						coreContext);
+				httpexecutor.postProcess(response, httpproc, coreContext);
+
+				System.out.println("<< Response: " + response.getStatusLine());
+//				System.out.println(EntityUtils.toString(response.getEntity()));
+				InputStream is = response.getEntity().getContent();
+				ByteArrayOutputStream outSteam = new ByteArrayOutputStream();  
+			    byte[] buffer = new byte[10240];  
+			    int len = -1;  
+			    int ii = 0;
+			    while ((len = is.read(buffer)) != -1) {  
+			        outSteam.write(buffer, 0, len);  
+			        ii++;
+			        System.out.println("the " + ii + " time length : " + len);
+			    }  
+			    outSteam.close();  
+			    is.close();  
+			    System.out.println(outSteam.toByteArray());
+			    
+				System.out.println("XXOO");
+				
+				System.out.println();
+				System.out.println("==============");
+				if (!connStrategy.keepAlive(response, coreContext)) {
+					conn.close();
+				} else {
+					System.out.println("Connection kept alive...");
+				}
+			}
+		} finally {
+			conn.close();
+		}
+	}
+
+}
