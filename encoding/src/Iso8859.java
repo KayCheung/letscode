ae0cd7cf904747dc04d4b629ccd3fa96a0ae67ae
marvin <marvin@marvin-desktop>
2015-05-19 20:42:15 +0800

    a

---
+++ encoding/src/Iso8859.java
@@ -0,0 +1,30 @@
+
+
+import java.nio.charset.Charset;
+
+public class Iso8859 {
+	public static final String ISO_8859 = "iso-8859-";
+
+	public static void displayChar() {
+		// 224 -- 1110 0000 -->-32
+		byte b = (byte) -32;
+		int total = 16;
+		for (int i = 1; i <= total; i++) {
+			String charsetname = ISO_8859 + i;
+			String str;
+			try {
+				str = new String(new byte[] { b },
+						Charset.forName(charsetname));
+			} catch (Exception e) {
+				System.out.println("224--" + charsetname + "-->not supported");
+				continue;
+			}
+			System.out.println("224--" + charsetname + "-->" + str);
+		}
+	}
+
+	public static void main(String[] args) {
+		displayChar();
+	}
+
+}
