da439b7674f7efce297f41465cfe67ef2d5c7dd3
libing2 <libing2@tuniu.com>
2015-12-13 12:24:15 +0800

    a

--- datuniu/understandlambda/src/com/tuniu/understand/rule4default/ClassMethodPreferred.java
+++ datuniu/understandlambda/src/com/tuniu/understand/rule4default/ClassMethodPreferred.java
@@ -4,38 +4,28 @@ package com.tuniu.understand.rule4default;
  * Created by nuc on 2015/11/17.
  */
 interface C {
-    void mA();
-
-    default void mB() {
+    default void m() {
         System.out.println("C.mB");
     }
 }
 
-abstract class CImpl implements C {
-    @Override
-    public void mA() {
-        System.out.println("CImpl.mA");
+class Concrete {
+    public void m() {
+        System.out.println("Concrete.mB");
     }
-
-    public abstract void mB();
-
 }
 
-public class ClassMethodPreferred extends CImpl {
-    @Override
-    public void mA() {
-        System.out.println("ClassMethodPreferred.mA");
-    }
 
+public class ClassMethodPreferred extends Concrete implements C {
     // class中 同样签名的方法（即使是abstract的），总是优于 接口的default method
-    // Hence the default keyword: default methods are a fallback if the class hierarchy doesn't say anything
+    // Hence the default keyword: default methods are a FALLBACK if the class hierarchy doesn't say anything
     @Override
-    public void mB() {
+    public void m() {
         System.out.println("ClassMethodPreferred.mB");
     }
 
     public static void main(String[] args) {
         C c = new ClassMethodPreferred();
-        c.mB();
+        c.m();
     }
 }
