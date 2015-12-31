e1618637a8002706f71ae768e7cb8a0485067017
marvin263 <marvin263@163.com>
2015-12-31 21:21:53 +0800

    a

--- tntrip/understandlambda/src/RecoverBST.java
+++ tntrip/understandlambda/src/RecoverBST.java
@@ -1,5 +1,3 @@
-import apple.laf.JRSUIUtils;
-
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
@@ -37,7 +35,7 @@ public class RecoverBST {
         while (!(cur == null && stack.isEmpty())) {
             if (cur != null) {
                 stack.push(cur);
-                stackAtLeft.push(selfAtLeft?Boolean.TRUE:Boolean.FALSE);
+                stackAtLeft.push(selfAtLeft ? Boolean.TRUE : Boolean.FALSE);
 
                 cur = cur.left;
                 selfAtLeft = true;
@@ -45,7 +43,7 @@ public class RecoverBST {
                 cur = stack.pop();
 
                 rst.add(cur.val);
-                
+
 
                 cur = cur.right;
                 selfAtLeft = false;
@@ -54,73 +52,24 @@ public class RecoverBST {
         return rst;
     }
 
-    public static class MistakePair {
-        public static class RecoverNode {
-            TreeNode parent;
-            boolean selfAtLeft;
-            TreeNode self;
+    public static class RecoverNode {
+        TreeNode parent;
+        boolean selfAtLeft;
+        TreeNode self;
 
-            public RecoverNode(TreeNode parent, TreeNode self, boolean selfAtLeft) {
-                this.parent = parent;
-                this.self = self;
-                this.selfAtLeft = selfAtLeft;
-            }
-        }
-
-        RecoverNode left;
-        RecoverNode right;
-
-        public MistakePair(RecoverNode left, RecoverNode right) {
-            this.left = left;
-            this.right = right;
+        public RecoverNode(TreeNode parent, TreeNode self, boolean selfAtLeft) {
+            this.parent = parent;
+            this.self = self;
+            this.selfAtLeft = selfAtLeft;
         }
     }
 
-
-    private MistakePair.RecoverNode[] calcExchangeRecoverNode(MistakePair[] mistakePairs) {
-        if (mistakePairs.length == 0) {
-            return new MistakePair.RecoverNode[0];
-        }
-        if (mistakePairs.length == 1) {
-            return new MistakePair.RecoverNode[]{mistakePairs[0].left, mistakePairs[0].right};
-        }
-        // being here mistakePairs.length==2
-        MistakePair f = mistakePairs[0];
-        MistakePair s = mistakePairs[1];
-        //f0,f1,s0,s1
-        //s0,f1,f0,s1
-        if (s.left.self.val >= f.right.self.val &&
-                f.right.self.val >= f.left.self.val &&
-                f.left.self.val >= s.right.self.val) {
-            return new MistakePair.RecoverNode[]{f.left, s.left};
-        }
-        //s1,f1,s0,f0
-        if (s.right.self.val >= f.right.self.val &&
-                f.right.self.val >= s.left.self.val &&
-                s.left.self.val >= f.left.self.val) {
-            return new MistakePair.RecoverNode[]{f.left, s.right};
-        }
-        //f0,s0,f1,s1
-        if (f.left.self.val >= s.left.self.val &&
-                s.left.self.val >= f.right.self.val &&
-                f.right.self.val >= s.right.self.val) {
-            return new MistakePair.RecoverNode[]{f.right, s.left};
-        }
-        //f0,s1,s0,f1
-        if (f.left.self.val >= s.right.self.val &&
-                s.right.self.val >= s.left.self.val &&
-                s.left.self.val >= f.right.self.val) {
-            return new MistakePair.RecoverNode[]{f.right, s.right};
-        }
-        return new MistakePair.RecoverNode[0];
-    }
-
-    private void doExchange(MistakePair.RecoverNode[] recoverNodes) {
-        if (recoverNodes.length != 2) {
+    private void doExchange(RecoverNode[] rns) {
+        if (rns.length != 2) {
             return;
         }
-        MistakePair.RecoverNode rn0 = recoverNodes[0];
-        MistakePair.RecoverNode rn1 = recoverNodes[1];
+        RecoverNode rn0 = rns[0];
+        RecoverNode rn1 = rns[1];
 
         if (rn0.parent == rn1.self || rn1.parent == rn0.self) {
             exchangeAdjacent(rn0, rn1);
@@ -129,7 +78,7 @@ public class RecoverBST {
         }
     }
 
-    private void exchangeFarAway(MistakePair.RecoverNode rn0, MistakePair.RecoverNode rn1) {
+    private void exchangeFarAway(RecoverNode rn0, RecoverNode rn1) {
         TreeNode tn0 = rn0.self;
         TreeNode tn1 = rn1.self;
         TreeNode tmp = null;
@@ -157,7 +106,7 @@ public class RecoverBST {
     }
 
 
-    private void exchangeAdjacent(MistakePair.RecoverNode rn0, MistakePair.RecoverNode rn1) {
+    private void exchangeAdjacent(RecoverNode rn0, RecoverNode rn1) {
 
         TreeNode rn0_R = rn0.self.right;
         TreeNode rn0_L = rn0.self.left;
@@ -166,21 +115,21 @@ public class RecoverBST {
         TreeNode rn1_L = rn1.self.left;
 
         if (rn0.parent == rn1.self) {
-            MistakePair.RecoverNode rn_high = rn1;
-            MistakePair.RecoverNode rn_low = rn0;
+            RecoverNode rn_high = rn1;
+            RecoverNode rn_low = rn0;
             TreeNode rn_low_R = rn0_R;
             TreeNode rn_low_L = rn0_L;
             doExchangeAdjacent(rn_high, rn_low, rn_low_R, rn_low_L);
         } else if (rn1.parent == rn0.self) {
-            MistakePair.RecoverNode rn_high = rn0;
-            MistakePair.RecoverNode rn_low = rn1;
+            RecoverNode rn_high = rn0;
+            RecoverNode rn_low = rn1;
             TreeNode rn_low_R = rn1_R;
             TreeNode rn_low_L = rn1_L;
             doExchangeAdjacent(rn_high, rn_low, rn_low_R, rn_low_L);
         }
     }
 
-    private void doExchangeAdjacent(MistakePair.RecoverNode rn_high, MistakePair.RecoverNode rn_low,
+    private void doExchangeAdjacent(RecoverNode rn_high, RecoverNode rn_low,
                                     TreeNode rn_low_R, TreeNode rn_low_L) {
         //change rn0.self parent
         if (rn_high.selfAtLeft) {
@@ -218,10 +167,10 @@ public class RecoverBST {
         tn1.left = tn2;
         tn2.right = tn3;
 
-        MistakePair.RecoverNode rn0 = new MistakePair.RecoverNode(tn1, tn2, true);
-        MistakePair.RecoverNode rn1 = new MistakePair.RecoverNode(tn1, tn3, false);
+        RecoverNode rn0 = new RecoverNode(tn1, tn2, true);
+        RecoverNode rn1 = new RecoverNode(tn1, tn3, false);
 
-        rbst.doExchange(new MistakePair.RecoverNode[]{rn0, rn1});
+        rbst.doExchange(new RecoverNode[]{rn0, rn1});
         System.out.println(tn1);
 
     }
