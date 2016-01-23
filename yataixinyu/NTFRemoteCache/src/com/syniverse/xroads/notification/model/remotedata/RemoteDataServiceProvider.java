90d0266e67009fa4f377c491ee7991c911046429
libing2 <libing2@tuniu.com>
2016-01-23 11:32:42 +0800

    delete redundant

--- yataixinyu/NTFRemoteCache/src/com/syniverse/xroads/notification/model/remotedata/RemoteDataServiceProvider.java
+++
@@ -1,8 +0,0 @@
-package com.syniverse.xroads.notification.model.remotedata;
-
-public class RemoteDataServiceProvider {
-	public static RemoteDataService getService() {
-		RemoteDataService INSTANCE = RemoteDataServiceImpl.getService();
-		return INSTANCE;
-	}
-}
--- yataixinyu/NTFRemoteCache/src/com/syniverse/xroads/notification/model/remotedata/XroadsUserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.syniverse.xroads.notification.model.remotedata;
-
-public class XroadsUserInfo extends UserInfo {
-	private static final long serialVersionUID = 1L;
-	@Override
-	public String toString() {
-		return "XroadsUserInfo--" + super.toString();
-	}
-}
