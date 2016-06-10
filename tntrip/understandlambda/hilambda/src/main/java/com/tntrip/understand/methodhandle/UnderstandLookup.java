091984970d95ebda4b703ef7df272e0e5098b437
libing2 <libing2@tuniu.com>
2016-06-10 17:12:11 +0800

    gradle

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/methodhandle/UnderstandLookup.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/methodhandle/UnderstandLookup.java
@@ -1,13 +1,13 @@
 package com.tntrip.understand.methodhandle;
 
-import sun.reflect.Reflection;
+//import sun.reflect.Reflection;
 
 /**
  * Created by nuc on 2015/11/22.
  */
 public class UnderstandLookup {
     public static void callerClass() {
-        System.out.println(Reflection.getCallerClass());
+        //System.out.println(Reflection.getCallerClass());
     }
 
     public static void main(String[] args) {
