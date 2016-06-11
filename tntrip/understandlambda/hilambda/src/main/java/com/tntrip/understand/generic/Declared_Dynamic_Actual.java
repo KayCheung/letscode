a22348a3df591c0f1d9b9d3b91fa8c0e7ffdd42c
marvin263 <marvin263@163.com>
2016-06-11 16:26:20 +0800

    Field    getGenericType是声明时的类型，包含泛型的；getType也是声明时的类型，但，不带有泛型信息

--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/generic/Declared_Dynamic_Actual.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/generic/Declared_Dynamic_Actual.java
@@ -4,37 +4,14 @@ package com.tntrip.understand.generic;
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by nuc on 2016/6/11.
  */
 public class Declared_Dynamic_Actual<E> {
-    /**
-     * <pre>
-     * declared type: Entry<E>
-     * actual type: Entry。正是由于 type erasure，actual type 总是 raw type
-     * Entry<E>是 parameterized type（而不是 raw type）
-     * first的declared type是Entry<E>，这个 Entry<E> 是 generict type Node<E> 的一个实例；并不是 raw type Entry的实例
-     *
-     * first的actual type是 raw type Entry；Entry本身是generic type，因为Entry有 formal type parameter
-     *
-     * </pre>
-     */
-    private Node<E> first = new Node<>(null, null, null);
-    private List<Integer> second = new ArrayList<>();
-
-    private static class Node<N> {
-        N item;
-        Node<N> next;
-        Node<N> prev;
-
-        Node(Node<N> prev, N element, Node<N> next) {
-            this.item = element;
-            this.next = next;
-            this.prev = prev;
-        }
-    }
+    private List<Integer> second = new ArrayList<Integer>();
 
     public static void kindsOfType() throws Exception {
         Field field = Declared_Dynamic_Actual.class.getDeclaredField("second");
@@ -48,11 +25,30 @@ public class Declared_Dynamic_Actual<E> {
         System.out.println("------------getClass------------");
         Class<? extends List> aClass = new Declared_Dynamic_Actual().second.getClass();
         AnalyzeType.analyzeType(aClass);
+    }
 
+    public static void collections_empty_list() throws Exception {
+        // public static final List EMPTY_LIST = new EmptyList<>();
+        // class EmptyList<E> extends AbstractList<E> implements RandomAccess, Serializable
+        Field field = Collections.class.getDeclaredField("EMPTY_LIST");
+
+        System.out.println("------------getGenericType------------");
+        AnalyzeType.analyzeType(field.getGenericType());
+
+        Type gettype = field.getType();
+        System.out.println("------------getType------------");
+        AnalyzeType.analyzeType(gettype);
+
+        System.out.println("------------getClass------------");
+        Class<? extends List> aClass = Collections.emptyList().getClass();
+        AnalyzeType.analyzeType(aClass);
 
     }
 
+
     public static void main(String[] args) throws Exception {
         kindsOfType();
+        System.out.println("\n");
+        collections_empty_list();
     }
 }
