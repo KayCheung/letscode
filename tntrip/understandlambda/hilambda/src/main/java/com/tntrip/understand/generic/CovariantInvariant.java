6352b8da37af605c0d6da8ef7c48feb670ed2e01
libing2 <libing2@tuniu.com>
2016-07-10 13:27:00 +0800

    Covariant和Invariant

---
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/generic/CovariantInvariant.java
@@ -0,0 +1,10 @@
+package com.tntrip.understand.generic;
+
+/**
+ * Created by libing2 on 2016/7/10.
+ */
+public class CovariantInvariant {
+    public static void main(String[] args) {
+        
+    }
+}
--- tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/generic/MyObject.java
+++ tntrip/understandlambda/hilambda/src/main/java/com/tntrip/understand/generic/MyObject.java
@@ -1,8 +1,38 @@
 package com.tntrip.understand.generic;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by libing2 on 2016/5/31.
  */
 public class MyObject {
+    public static void main(String[] args) {
+        List rawList = new ArrayList();
+        List<Object> objList = new ArrayList<>();
+        List<String> strList = new ArrayList<>();
 
+        unsafeAdd_Raw(strList, new Integer(42));
+
+        rawList = objList;
+        rawList = strList;
+
+        //objList = strList;
+        //unsafeAdd_Object(strList, new Integer(42));
+
+    }
+
+    private static void unsafeAdd_Object(List<Object> objects, Object obj) {
+
+    }
+
+    private static void unsafeAdd_Raw(List rawList, Object obj) {
+
+    }
+    private static void wildcardSet(Set<?> set1, Set<?> set2) {
+        for (Object o : set1) {
+
+        }
+    }
 }
