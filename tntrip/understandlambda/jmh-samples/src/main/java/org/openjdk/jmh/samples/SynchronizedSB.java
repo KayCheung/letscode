ac8d6dfc07382f4e5af90d251b35b41da0397e23
marvin263 <marvin263@163.com>
2016-12-08 01:40:34 +0800

    单线程中，使用synchronized，依然会降低性能

---
+++ tntrip/understandlambda/jmh-samples/src/main/java/org/openjdk/jmh/samples/SynchronizedSB.java
@@ -0,0 +1,18 @@
+package org.openjdk.jmh.samples;
+
+/**
+ * Created by nuc on 2016/12/8.
+ */
+public class SynchronizedSB {
+    private StringBuilder sb = new StringBuilder();
+
+    public synchronized int length() {
+        return sb.length();
+    }
+    public synchronized StringBuilder append(String str) {
+        return sb.append(str);
+    }
+    public synchronized StringBuilder delete(int start, int end) {
+        return sb.delete(start, end);
+    }
+}
